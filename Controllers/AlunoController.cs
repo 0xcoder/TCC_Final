0f4102fbdfbf1fc3192399d7042544a334b20ba7
Estevao Rodrigues <estevaorssouza@hotmail.com>
2021-10-19 20:54:46 -0300

    1.0
    1.0

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,66 @@
+﻿using Gerenciador.Context;
+using Gerenciador.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gerenciador.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private readonly Contexto _context;
+        public AlunoController(Contexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.aluno.Where(a=>a.idUsuario == User.Identity.Name).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Aluno aluno)
+        {
+            var Aaluno = new Aluno
+            {
+                Nome = aluno.Nome,
+                Disciplina = aluno.Disciplina,
+                Descricao = aluno.Descricao,
+                Tema = aluno.Tema,
+                Integrantes = aluno.Integrantes,
+                idUsuario = User.Identity.Name,
+                tipoPasta = "privada"
+            };
+            _context.Add(Aaluno);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ContentPaste(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var verifica = await _context.aluno.FindAsync(id);
+
+            if (verifica.idUsuario !=User.Identity.Name)
+            {
+                return RedirectToAction("Index");
+            }
+            var conteudo = await _context.FilesOnDatabase.Where(a=>a.idUsuario == verifica.idUsuario).ToListAsync();
+            return View(conteudo);
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gerenciador.Models
+{
+    public class Aluno
+    {
+      [Key]
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Insira o Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Nome da Disciplina")]
+
+        public string Disciplina { get; set; }
+
+        [Required(ErrorMessage ="Insira uma descrição")]
+
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Insira um Tema")]
+
+        public string Tema { get; set; }
+
+        [Required(ErrorMessage = "Insira os Integrantes")]
+        
+        public string Integrantes { get; set; }
+        
+        public string idUsuario { get; set; }
+        public string tipoPasta { get; set; }
+
+
+    }
+}
